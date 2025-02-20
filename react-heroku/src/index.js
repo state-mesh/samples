eff643fd5aa5833fe98b78118f4f72f4c9a463b4
dan.chirila <dan.chirila@m-ware.eu>
2025-02-20 12:54:23 +0200

    Test change 4

--- react-heroku/src/index.js
+++ react-heroku/src/index.js
@@ -4,7 +4,7 @@ class App extends React.Component{
     render(){
         return(
             <div>
-                Hello World API!
+                Hello World API - change 1!
             </div>
         )
     }
